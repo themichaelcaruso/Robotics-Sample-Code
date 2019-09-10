8bd2bf7fd9a0102f25f55cf1ac9ec28489001760
caek88 <35225849+caek88@users.noreply.github.com>
2019-09-10 13:33:26 -0400

    Update Controller Intro.cpp

--- Controller/Controller Intro.cpp
+++ Controller/Controller Intro.cpp
@@ -1,1 +1,20 @@
+//This file is an intro to using a controller to control your robot
 
+//To make a controller object:
+controller ctr = controller();
+
+//Using Buttons:
+ctr.ButtonA.pressing() //This returns true or false for whether Button A is being pressed or not
+//Each Button on the controller is labeled
+
+//Using Joysticks:
+ctr.Axis1.position(positionUnits::pct) //This returns an integer value for the position of the 1 Axis on the controller
+//In the middle, the position is 0
+  
+//Using the LCD:
+//You can print three lines of text to the controller screen, similar to the brain screen.
+
+ctr.Screen.clearScreen() //This clears the screen
+ctr.Screen.clearLine(1) //This clears line 1
+ctr.Screen.setCursor(1,0) //This sets the cursor to 1,0 or the top left corner
+ctr.Screen.print("Robots will rise") //This prints a message to the screen
