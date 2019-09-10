a3845c0e49b39bd3f30c4e28472efa9fba5c712b
caek88 <35225849+caek88@users.noreply.github.com>
2019-09-10 13:12:57 -0400

    Update Maze Program.cpp

--- Motors/Maze Program.cpp
+++ Motors/Maze Program.cpp
@@ -1,21 +1,22 @@
-#include "robot-config.h"
-using namespace vex;
+//This program is a sample program for navigating a maze using the startRotateTo() command
+using namespace vex;//Reference the vex class
+brain Brain;//Define brain and motors
 motor mtrLeft = motor(PORT1, false);
 motor mtrRight = motor(PORT2, true);
-void driveTo(double leftDeg, double rightDeg){
-    mtrLeft.startRotateTo(leftDeg, rotationUnits::deg, 40, velocityUnits::pct);
+void driveTo(double leftDeg, double rightDeg){//This is a function that takes degrees to move to and moves the motors to those degrees
+    mtrLeft.startRotateTo(leftDeg, rotationUnits::deg, 40, velocityUnits::pct);//Start the motors rotating to the inputted values
     mtrRight.startRotateTo(rightDeg, rotationUnits::deg, 40, velocityUnits::pct);
-    while (true){
+    while (true){//Wait for both motors to stop spinning
         if (!mtrLeft.isSpinning() && !mtrRight.isSpinning()){
             break;
         }
     }
-    task::sleep(50);
+    task::sleep(50);//Wait 50 milliseconds for the motors to stop
 }
-int main(){
-    mtrLeft.resetRotation();
+int main(){//Main method
+    mtrLeft.resetRotation();//Reset motor rotations
     mtrRight.resetRotation();
-    driveTo(1014, 1014);
+    driveTo(1014, 1014);//Drive to each position
     driveTo(742, 1282);
     driveTo(1277, 1816);
     driveTo(1006, 2075);
