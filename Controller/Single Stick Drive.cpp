4ab849f6ef5baf46929ab5617057715da31ae365
caek88 <35225849+caek88@users.noreply.github.com>
2019-09-10 13:22:53 -0400

    Update Single Stick Drive.cpp

--- Controller/Single Stick Drive.cpp
+++ Controller/Single Stick Drive.cpp
@@ -1,12 +1,13 @@
+//This program is for a single stick driven robot
 using namespace vex;
 brain Brain;
-motor mtrLeft = motor(PORT1, false);
+motor mtrLeft = motor(PORT1, false);//Create the motors
 motor mtrRight = motor(PORT2, true);
-controller ctr = controller(controllerType::primary);
+controller ctr = controller(controllerType::primary);//Create the controller
 int main() {
     while (true){
-        int y = ctr.Axis3.position(percentUnits::pct);
-        int x = ctr.Axis4.position(percentUnits::pct);
+        int y = ctr.Axis3.position(percentUnits::pct);//Get the position of the controller for forward and back
+        int x = ctr.Axis4.position(percentUnits::pct);//Get the position of the controller for Right and Left
         mtrLeft.spin(directionType::fwd, y+x, percentUnits::pct);
         mtrRight.spin(directionType::fwd, y-x, percentUnits::pct);
 
