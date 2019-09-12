e59e237e06a0ffbff22df315ff17a61c4f60429c
caek88 <35225849+caek88@users.noreply.github.com>
2019-09-12 13:52:37 -0400

    Update Potentiometer.cpp

--- Sensors/Potentiometer.cpp
+++ Sensors/Potentiometer.cpp
@@ -1,1 +1,17 @@
-
+//This program is for using the potentiometer
+//A potentiometer is used to measure how far an axle has turned and has a limited range of turning (It will break if you turn it too far)
+//This is an analog sensor
+using namespace vex;
+brain Brain;
+pot potFoo = pot(Brain.ThreeWirePort.A);//Create the potentiometer object on three wire port A
+int main() {
+    while (true){
+        //Print the gyro's value to the brain screen
+        Brain.Screen.clearScreen();
+        Brain.Screen.setCursor(1,1);
+        Brain.Screen.print("%d\n", potFoo.value(percentUnits::pct));//Measure as a percentage
+        Brain.Screen.print("%d\n", potFoo.value(rotationUnits::deg));//Measure as an angle
+        Brain.Screen.print("%d", potFoo.value(analogUnits::mV));//Measure in millivolts
+        task::sleep(10);
+    }
+}
