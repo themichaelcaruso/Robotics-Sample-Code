e187dbd4c77ae6f6443142375cac424243647bf7
themichaelcaruso <45773009+themichaelcaruso@users.noreply.github.com>
2019-09-16 12:02:49 -0400

    Update Moving Straight.cpp

--- Motors/Moving Straight.cpp
+++ Motors/Moving Straight.cpp
@@ -1,9 +1,9 @@
 #include "robot-config.h" //boilerplate
           
 /*
-    Sets both motors to rotate for 1 second
+    Sets both motors to rotate for 1 second, the time functions are not blocking (the code will not wait for the rotation to finish)
 */
 int main() {
-    M1.startRotateFor(1,timeUnits::sec);
+    M1.rotateFor(1,timeUnits::sec);
     M2.rotateFor(1,timeUnits::sec);
 }
