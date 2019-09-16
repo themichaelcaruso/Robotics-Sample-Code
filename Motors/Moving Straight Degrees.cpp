d78bb6afd7ffcf447f3a6e189363e780f226edde
themichaelcaruso <45773009+themichaelcaruso@users.noreply.github.com>
2019-09-16 11:56:18 -0400

    Update Moving Straight Degrees.cpp

--- Motors/Moving Straight Degrees.cpp
+++ Motors/Moving Straight Degrees.cpp
@@ -1,20 +1,6 @@
-//This program is for making the robot move straight with a two motor drive
-using namespace vex;//Reference the vex class
-
-//Create the motor objects for the left and right motors
-motor mtrLeft = motor(PORT1, false); //Set the left motor on port 1 with its direction not reversed
-motor mtrRight = motor(PORT2, true); //Set the right motor on port 2 with its direction reversed
+#include "robot-config.h"
 
 int main(){
-    //Make the robot drive straight for 720 degrees (2 rotations)
-    mtrLeft.resetRotation();//Reset the motor encoder rotations
-    mtrRight.resetRotation();
-    mtrLeft.spin(directionType::fwd, 100, percentUnits::pct);
-    mtrRight.spin(directionType::fwd, 100, percentUnits::pct);//Set each motor to 100% power forwards
-    
-    while (mtrLeft.rotation(rotationUnits::deg) < 720){
-        task::sleep(25);//Check every 25 milliseconds
-    }//wait until the left motor has gone 720 degrees
-    mtrLeft.stop();
-    mtrRight.stop();//Stop both motors (They are still spinning)
+    M1.rotateFor(720, rotationUnits::deg);
+    M2.rotateFor(720, rotationUnits::deg);
 }
